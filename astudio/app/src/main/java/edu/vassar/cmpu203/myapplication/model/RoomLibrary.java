836a913560491ac9c1af1a4141a42861055f41eb
alexandraguillen <alexandraguillen@vassar.edu>
2023-11-13 11:14:47 -0500

    Modified RoomLibrary

--- astudio/app/src/main/java/edu/vassar/cmpu203/myapplication/model/RoomLibrary.java
+++ astudio/app/src/main/java/edu/vassar/cmpu203/myapplication/model/RoomLibrary.java
@@ -1,23 +1,32 @@
 package edu.vassar.cmpu203.myapplication.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Set;
 
 public class RoomLibrary {
     public ArrayList<Room> rooms;
 
 
-    public int size = 0;
+    public int size = 1;
 
     public RoomLibrary(){
         this.rooms = new ArrayList<>();
     }
+
+    Room[] roomList = new Room[] {
+            new Room(House.JEWETT,3, RoomType.SINGLE, true)};
+
 
     public void addRoom(House name, int floor, RoomType rt, boolean availability){
         Room r = new Room(name, floor, rt, availability);
         this.rooms.add(r);
         this.size ++;
     }
+
+    public ArrayList<Room> getRoomLibrary(){
+        this.rooms = (ArrayList<Room>) Arrays.asList(roomList);
+        return this.rooms; }
 
 
     public String toString(){
